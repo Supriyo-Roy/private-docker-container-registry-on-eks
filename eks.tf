a5255c2b1816b953df39b502451bff642e9db96f
Supriyo Roy <supriyoroy@Supriyos-MacBook-Air.local>
2026-07-27 16:24:50 +0530

    eks

--- eks.tf
+++ eks.tf
@@ -5,6 +5,8 @@ module "eks" {
   name               = var.cluster_name
   kubernetes_version = "1.33"
   endpoint_public_access  = true
+  # The IAM role that Terraform Cloud assumes (OIDC / dynamic credentials / workspace credentials) is not automatically given access to the cluster.
+  enable_cluster_creator_admin_permissions = true
 
   # EKS Addons
   addons = {
