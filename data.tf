ec936dc69633a895785cc988cdb746c9b3a19fa1
Supriyo Roy <supriyoroy@Supriyos-MacBook-Air.local>
2026-07-28 01:01:09 +0530

    eks

--- data.tf
+++ data.tf
@@ -1,4 +1,4 @@
-data "aws_eks_cluster" "cluster" {
-  name = var.cluster_name
-  # depends_on = [ module.eks ]
-}
+# data "aws_eks_cluster" "cluster" {
+#   name = var.cluster_name
+#   # depends_on = [ module.eks ]
+# }
