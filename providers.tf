031782030e626777576debfc57bd115ba367407c
Supriyo Roy <supriyoroy@Supriyos-MacBook-Air.local>
2026-07-27 16:54:50 +0530

    eks

--- providers.tf
+++ providers.tf
@@ -35,3 +35,20 @@ provider "helm" {
   }
 }
 
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      module.eks.cluster_name,
+      "--region",
+      var.aws_region
+    ]
+  }
+}
