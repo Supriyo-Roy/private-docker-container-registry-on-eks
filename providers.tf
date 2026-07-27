8f69e7e5e7749ab9e347000d72a6a650a1efd607
Supriyo Roy <supriyoroy@Supriyos-MacBook-Air.local>
2026-07-27 15:56:45 +0530

    eks

--- providers.tf
+++ providers.tf
@@ -26,10 +26,10 @@ provider "aws" {
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    cluster_ca_certificate = base64decode(dmodule.eks.certificate_authority_data)
     exec {
       api_version = "client.authentication.k8s.io/v1"
-      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
       command     = "aws"
     }
   }
