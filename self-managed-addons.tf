6fdb2e261e4430e4bf1de30474f774af87df4538
Supriyo Roy <supriyoroy@Supriyos-MacBook-Air.local>
2026-07-27 11:11:01 +0530

    eks

--- self-managed-addons.tf
+++ self-managed-addons.tf
@@ -1,16 +1,27 @@
+# 1. Fetch official policy JSON
+data "http" "lb_controller_iam_policy" {
+  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json"
+}
+
+# 2. Declare the missing policy resource
+resource "aws_iam_policy" "lb_controller" {
+  name        = "AWSLoadBalancerControllerIAMPolicy"
+  path        = "/"
+  description = "IAM policy for AWS Load Balancer Controller"
+  policy      = data.http.lb_controller_iam_policy.response_body
+}
+
+# 3. Role setup for EKS Pod Identity
 resource "aws_iam_role" "lb_controller" {
   name = "AmazonEKSLoadBalancerControllerRole"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-
     Statement = [{
       Effect = "Allow"
-
       Principal = {
         Service = "pods.eks.amazonaws.com"
       }
-
       Action = [
         "sts:AssumeRole",
         "sts:TagSession"
@@ -19,6 +30,7 @@ resource "aws_iam_role" "lb_controller" {
   })
 }
 
+# 4. Attach the created policy to the role
 resource "aws_iam_role_policy_attachment" "lb_controller" {
   role       = aws_iam_role.lb_controller.name
   policy_arn = aws_iam_policy.lb_controller.arn
@@ -32,12 +44,10 @@ resource "aws_eks_pod_identity_association" "lb_controller" {
 }
 
 resource "helm_release" "aws_load_balancer_controller" {
-
   name       = "aws-load-balancer-controller"
   repository = "https://aws.github.io/eks-charts"
   chart      = "aws-load-balancer-controller"
-
-  namespace = "kube-system"
+  namespace  = "kube-system"
 
   set {
     name  = "clusterName"
